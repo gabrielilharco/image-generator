a68b6745484cb143617903beba1eda7d18efa1fa
Muzio <ax.muzio@gmail.com>
2016-08-22 09:41:31 -0300

    Creating Cmakefiles

---
+++ src/raytracer/RayTracer.cpp
@@ -0,0 +1,5 @@
+//
+// Created by muzio on 8/22/16.
+//
+
+#include "RayTracer.h"
---
+++ src/raytracer/RayTracer.h
@@ -0,0 +1,16 @@
+//
+// Created by muzio on 8/22/16.
+//
+
+#ifndef IMAGE_GENERATOR_RAYTRACER_H
+#define IMAGE_GENERATOR_RAYTRACER_H
+
+class RayTracer {
+public:
+
+private:
+
+};
+
+
+#endif //IMAGE_GENERATOR_RAYTRACER_H
