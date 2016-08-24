b185515ff250516c7fd16981b41ad7b5f3e113e6
gabrielilharco <gabriel.ilharco@gmail.com>
2016-08-24 14:15:27 -0300

    first intersection for world scene

--- src/shared/WorldScene.cpp
+++ src/shared/WorldScene.cpp
@@ -8,6 +8,15 @@ void WorldScene::addObject(Object *obj) {
     listOfObjects.push_back(obj);
 }
 
-bool WorldScene::getFirstIntersection(const Ray &ray, Object *obj) {
-    return false;
+double WorldScene::getFirstIntersection(const Ray &ray, Object* obj) {
+    double distanceToFirst = -1;
+    double currentDistance;
+    for (int i = 0; i < listOfObjects.size(); i++) {
+        currentDistance = listOfObjects[i]->getFirstIntersection(ray);
+        if (distanceToFirst == -1 || currentDistance < distanceToFirst) {
+            distanceToFirst = currentDistance;
+            obj = listOfObjects[i];
+        }
+    }
+    return distanceToFirst;
 }
