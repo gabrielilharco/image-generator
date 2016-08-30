d902b54fa52b72c23bd749e1bed22442ad76b3e4
Matheus Leao <leaomatheus11@gmail.com>
2016-08-30 01:58:06 -0300

    Correcting function that checks if point is inside triangle

--- src/rasterizer/geometry/TriangleProjection.cpp
+++ src/rasterizer/geometry/TriangleProjection.cpp
@@ -1,16 +1,16 @@
 #include <shared/math/Vector2.h>
 #include "rasterizer/geometry/TriangleProjection.h"
+#include <math.h>
 
 TriangleProjection::TriangleProjection(const Vector2 a, const Vector2 b, const Vector2 c, const Triangle correspondentTriangle)
     : a(a), b(b), c(c), correspondentTriangle(correspondentTriangle) {}
 
 bool TriangleProjection::isInside(Vector2 point) {
-    double l1 = point.cross(b-a);
-    double l2 = point.cross(c-b);
-    double l3 = point.cross(a-c);
+    double l1 = (point-a).cross(b-a);
+    double l2 = (point-b).cross(c-b);
+    double l3 = (point-c).cross(a-c);
 
     if(l1 >= 0 && l2 >= 0 && l3 >= 0) return true;
-    if(l1 <= 0 && l2 <= 0 && l3 <= 0) return true;
+    if(l1 < 0 && l2 < 0 && l3 < 0) return true;
     return false;
 }
-
